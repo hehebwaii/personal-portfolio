0c61661b15ce0d45fdfa7152bacb0880e92eb2b0
hehebwaii <142102178+hehebwaii@users.noreply.github.com>
2025-01-21 21:36:08 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>Hi! I'm [Your Name], a passionate developer focused on creating impactful solutions with technology.</p>
+    </div>
+  );
+}
+
+export default About;
